efe015469c92d998b329abfe42f2e13cfae42b9f
Christofer Saputra <christofersaputra29@gmail.com>
2021-07-02 15:08:11 +0700

    Update docker-compose to wait for pg before starting server

--- controller/settings.py
+++ controller/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e14t5dtz@4kscc11at$xv*#$7$bly^gc&h0ca9&2z2$%wc_7!o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'controller.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
