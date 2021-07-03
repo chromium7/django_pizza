a34b4bae4776277741bfe54b721180d9609b4916
Christofer Saputra <christofersaputra29@gmail.com>
2021-07-03 11:21:03 +0700

    Add header, footer and home page

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_home_page_renders_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'shop/index.html')
