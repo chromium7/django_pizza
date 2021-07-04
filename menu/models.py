61d16fedcc94c5a0669eb84e07485ab2b7aaa26e
Christofer Saputra <christofersaputra29@gmail.com>
2021-07-04 21:28:28 +0700

    Create models for pizza objects

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,41 @@
+"""
+    Models for pizza tables
+    One pizza object can have several toppings
+    Each topping has its own specific type
+        e.g. Sauce = [Tomato, Chilli]
+"""
+
 from django.db import models
 
-# Create your models here.
+
+class ToppingType(models.Model):
+    name = models.CharField(max_length=32)
+    
+
+class Topping(models.Model):
+    name = models.CharField(max_length=32)
+    price = models.IntegerField()
+    type = models.ForeignKey(ToppingType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name.capitalise()} costs ${self.price}"
+
+
+class Pizza(models.Model):    
+    size = models.CharField(max_length=1)
+    price = models.IntegerField()
+    toppings = models.ManyToManyField(Topping)
+
+    def __str__(self):
+        if self.size == 's':
+            size = 'Small'
+        elif self.size == 'm':
+            size = 'Medium'
+        else:
+            size = 'Large'
+        toppings = ", ".join([topping.name for topping in self.toppings.all()])
+        return f"{size} pizza with {toppings} costs ${self.total_price()}"
+
+    def total_price(self):
+        toppings_price = sum([topping.price for topping in self.toppings.all()])
+        return self.price + toppings_price
